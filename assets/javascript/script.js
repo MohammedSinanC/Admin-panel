4119b4590973538211c4bba7c1adc093c931c725
MohammedSinanC <sinanruzz9@gmail.com>
2022-12-21 23:39:53 +0530

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+function onclickSign(){
+     var email = document.getElementById("form3Example3").value
+     var password = document.getElementById("form3Example4").value
+
+
+     if (email != "abcd@gmail.com"){
+          alert('Please enter valid email id')
+          return 0;
+     }
+     else if ( password != 123123 ){
+          alert('Please enter valid password')
+     }
+     else{
+          alert("Hello, Welcome")
+          window.location.href = '/admin.html'
+     }
+     window.onbeforeunload = function() { return "Your work will be lost."; };
+}
+function logoutFunction() {
+     alert('Logout Successfully')
+     window.location.href = '/index.html'
+}
